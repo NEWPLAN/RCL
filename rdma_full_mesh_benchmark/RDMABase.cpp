d487b832e189c409bcad032c73a9d8e9de6f3939
Dict Xiong <me@beardic.cn>
2020-11-04 16:23:28 +0800

    bug fix

--- rdma_full_mesh_benchmark/RDMABase.cpp
+++ rdma_full_mesh_benchmark/RDMABase.cpp
@@ -61,7 +61,7 @@ struct ibv_pd *RDMABase::rc_get_pd(struct rdma_cm_id *id)
     struct RDMAContext *ctx = (struct RDMAContext *)id->context;
     if (ctx == 0 || ctx == nullptr || ctx == NULL)
     {
-        LOG_INFO("Error of get pd: %x\n", ctx);
+        LOG(INFO) << "Error of get pd: " << hex << ctx;
         return NULL;
     }
     return ctx->pd;
