efef611f95144687e6ec4f6358a1b1c0e86b893b
Dict Xiong <me@beardic.cn>
2020-11-05 13:26:56 +0800

    debug

--- rdma_full_mesh_benchmark/RDMABenchmark.cpp
+++ rdma_full_mesh_benchmark/RDMABenchmark.cpp
@@ -198,7 +198,11 @@ void master_control(std::vector<std::string> ips, std::string master_ip)
         });
         std::this_thread::sleep_for(std::chrono::seconds(10));
         master->broadcast_imm(IMM_CLIENT_WRITE_START);
+        new std::thread([master](){
+            master->setup();
+        });
         timer.Start();
+        
     }
     while(true)
     {
